82ea84669be44514ce5c865ff56d04f753a60349
Abdul Raheem Abid <abdulraheem.abid@gmail.com>
2021-04-21 22:09:26 +0500

    style customise

--- src/app/@shared/base-components/rvn-toggle/rvn-toggle.component.ts
+++ src/app/@shared/base-components/rvn-toggle/rvn-toggle.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { CustomControlValueAccessor } from '../../utils/custom-control-value-accessor';
 import { RvnToggleInput } from './rvn-toggle.input';
 
 @Component({
   selector: 'rvn-toggle',
   templateUrl: './rvn-toggle.component.html',
-  styleUrls: ['./rvn-toggle.component.scss']
+  styleUrls: ['./rvn-toggle.component.scss'],
+  providers:[
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RvnToggleComponent),
+      multi: true
+    }
+  ]
 })
 export class RvnToggleComponent extends CustomControlValueAccessor implements OnInit {
 
   @Input() params: RvnToggleInput = null;
 
   ngOnInit() {
-    if (!this.params.required) this.params.required = false;
-    if (!this.params.requiredErrorMessage) this.params.requiredErrorMessage = `${this.params.label} is required`;
-    if (!this.params.styleVersion) this.params.styleVersion = 'v1';
+    if (!this.params?.required) this.params.required = false;
+    if (!this.params?.requiredErrorMessage) this.params.requiredErrorMessage = `${this.params.label} is required`;
+    if (!this.params?.styleVersion) this.params.styleVersion = 'v1';
   }
 
 }
